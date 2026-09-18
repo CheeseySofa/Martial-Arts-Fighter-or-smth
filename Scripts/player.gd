7491e7a1bf5908c5069496aec0a84ff461344360
Yajnesh Skandhan✌️ <145314113+CheeseySofa@users.noreply.github.com>
2026-09-18 21:21:17 +0530

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,6 +11,7 @@ var a = null
 var d = null
 var perm_a = null
 var perm_d = null
+var dashin = false
 
 
 func _physics_process(delta):
@@ -24,18 +25,18 @@ func _physics_process(delta):
 		var a1 = Input.is_action_just_pressed("A")
 		var d1 = Input.is_action_just_pressed("D")
 		if (a1 or d1) and Dash_window and cd:
-			print(a1)
-			print(d1)
-			print(perm_a)
-			print(perm_d)
+						
 			if a1 and perm_a:
-				print("Dash Left")
+				velocity.x -= 1400
 				$Dash_Cd.start()
 				cd = false
+				dashin = true
+				
 			elif d1 and perm_d:
-				$Dash_Cd.start()
+				velocity.x += 1400
 				print("dash right")
 				cd = false
+				dashin = true
 	
 	#air stuff
 	if not is_on_floor():
@@ -45,10 +46,16 @@ func _physics_process(delta):
 	if is_on_floor():
 		if direction:
 			time += delta
-			velocity.x = clamp((abs(velocity.x) + (110 * time)) ,300, SPEED) * direction
+			if !dashin:
+				velocity.x = clamp((abs(velocity.x) + (110 * time)) ,300, SPEED) * direction
+			else:
+				self.velocity = self.velocity.lerp(Vector2(0,0),time * 0.4)
+				if self.velocity.x == 0:
+					dashin = false
 		else:
 			time = 0
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if !dashin:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	#Inputs frrrr
 	if Input.is_action_just_pressed("jump") and is_on_floor():
